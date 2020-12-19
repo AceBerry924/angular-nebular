e7b27c5daca302a85c31070272eb85a451084583
aceberry924 <meobius123@gmail.com>
2020-12-20 00:18:17 +0800

    add details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,17 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
+  {
+    path: 'details',
+    loadChildren: () => import('./pages/details/details.module').then(m => m.DetailsModule)
+  },
+
+  // TODO: redirect everything else to home for now. Maybe create a not found page.
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload', enableTracing: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
