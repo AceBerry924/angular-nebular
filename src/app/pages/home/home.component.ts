a5bbc4ce21c6d4b414dd7a9ea61a7c8fb5c3c86f
aceberry924 <meobius123@gmail.com>
2020-12-20 02:13:24 +0800

    add check part when get error or body is null after get response

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NbDialogService } from '@nebular/theme';
+
+import { ErrorDialogComponent } from '../../components/error-dialog/error-dialog.component';
 
 import { APIService } from '../../services/api.service';
 import { PostService } from '../../services/post.service';
@@ -18,7 +21,8 @@ export class HomeComponent implements OnInit {
   constructor(
     private router: Router,
     private apiService: APIService,
-    private postService: PostService
+    private postService: PostService,
+    private dialogService: NbDialogService,
   ) { }
 
   ngOnInit(): void { }
@@ -29,12 +33,25 @@ export class HomeComponent implements OnInit {
     this.apiService.getPostData(this.postId).subscribe((data) => {
       this.loading = false;
 
+      if (!data.title || !data.body) {
+        this.open('Title or Body is missing from Post');
+        return;
+      }
+
       this.postService.setPost(data);
       this.router.navigate(['/details']);
 
     }, error => {
-      console.log('oops', error);
       this.loading = false;
+      this.open(error.message);
     })
+  }
+
+  open(err: string) {
+    this.dialogService.open(ErrorDialogComponent, {
+      context: {
+        error: err,
+      }
+    });
   }
 }
